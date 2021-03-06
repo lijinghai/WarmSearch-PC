9d104f109ba4c0010c17d5d40fb5db5402c11944
Dorian15 <68187839+Dorian1015@users.noreply.github.com>
2021-03-06 21:49:28 +0800

    前台代码

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,50 @@
+
 <template>
-  <div id="nav">
-    <header-nav></header-nav>
+  <div class="common-layout">
+    <el-container>
+      <el-header>Header</el-header>
+      <el-main>Main</el-main>
+    </el-container>
+
   </div>
-  <router-view/>
 </template>
 
-<style>
+<script>
 
+</script>
+
+<style>
+.el-header, .el-footer {
+  background-color: #B3C0D1;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
+
+.el-aside {
+  background-color: #D3DCE6;
+  color: #333;
+  text-align: center;
+  line-height: 200px;
+}
+
+.el-main {
+  background-color: #E9EEF3;
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+}
+
+body > .el-container {
+  margin-bottom: 40px;
+}
+
+.el-container:nth-child(5) .el-aside,
+.el-container:nth-child(6) .el-aside {
+  line-height: 260px;
+}
+
+.el-container:nth-child(7) .el-aside {
+  line-height: 320px;
+}
 </style>
