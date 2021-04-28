f12376bc760aaf4b361bcd3883041bf4c236ffd6
Dorian1015 <lijinghailjh@163.com>
2021-04-28 23:30:38 +0800

    2021 4.28 PC端发布页面添加

---
+++ src/views/PushFind/index.vue
@@ -0,0 +1,20 @@
+<!--
+ * @Description: 失物招领发布信息页面
+ * @Author: lijinghailjh@163.com
+ * @Date: 2021/4/28
+ -->
+<template>
+<div>
+  失物招领
+</div>
+</template>
+
+<script>
+export default {
+  name: "index"
+}
+</script>
+
+<style scoped>
+
+</style>
